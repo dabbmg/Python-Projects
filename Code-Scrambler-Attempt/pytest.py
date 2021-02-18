ddb4e2fe946c94f0d06b899fd7dc2edaba07cc77
dabbmg <dabb.mg@gmail.com>
2021-02-18 16:34:52 -0700

    update pytest
    using lists for encryption results and decryption results for easier code

--- Code-Scrambler-Attempt/pytest.py
+++ Code-Scrambler-Attempt/pytest.py
@@ -7,18 +7,14 @@ TESTS TO ENSURE PROPER FUNCTIONALITY
 def test_encryption_decryptionn():
     phrases = ["Dog", "Liver", "Four"] #Testing even and uneven words with multiple vowels
     ende = models.EncryptDecrypt()
-    encryptResult1 = ende.encrypt(phrases[0])
-    encryptResult2 = ende.encrypt(phrases[1])
-    encryptResult3 = ende.encrypt(phrases[2])
-    decryptResult1 = ende.decrypt(encryptResult1)
-    decryptResult2 = ende.decrypt(encryptResult2)
-    decryptResult3 = ende.decrypt(encryptResult3)
-    assert decryptResult1 == phrases[0]
-    assert decryptResult2 == phrases[1]
-    assert decryptResult3 == phrases[2]
-    if decryptResult1 != phrases[0] or decryptResult2 != phrases[1]:
+    encryptResults = [ende.encrypt(phrases[0]), ende.encrypt(phrases[1]), ende.encrypt(phrases[2])]
+    decryptResults = [ende.decrypt(encryptResults[0]), ende.decrypt(encryptResults[1]), ende.decrypt(encryptResults[2])]
+    assert decryptResults[0] == phrases[0]
+    assert decryptResults[1] == phrases[1]
+    assert decryptResults[2] == phrases[2]
+    if decryptResults[0] != phrases[0] or decryptResults[1] != phrases[1]:
         print("Uneven words(Dog, Liver) issue")
-    elif decryptResult3 != phrases[2]:
+    elif decryptResults[2] != phrases[2]:
         print("Even word(Four) issue")
     else:
         print("Encryption and Decryption: Pass")
