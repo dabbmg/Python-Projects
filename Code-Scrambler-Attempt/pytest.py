fa0f89684cdc628b8f15645a76d2f140b86cd306
dabbmg <dabb.mg@gmail.com>
2021-02-18 16:17:14 -0700

    update to pytest
    Using assert statements now for tests, and removed leftover comments

--- Code-Scrambler-Attempt/pytest.py
+++ Code-Scrambler-Attempt/pytest.py
@@ -6,27 +6,27 @@ TESTS TO ENSURE PROPER FUNCTIONALITY
 """
 def test_encryption_decryptionn():
     phrases = ["Dog", "Liver", "Four"] #Testing even and uneven words with multiple vowels
-    # print("Words to Test: " + phrases[0] + " - " + phrases[1] + " - " + phrases[2])
     ende = models.EncryptDecrypt()
     encryptResult1 = ende.encrypt(phrases[0])
     encryptResult2 = ende.encrypt(phrases[1])
     encryptResult3 = ende.encrypt(phrases[2])
-    # print("Encrypt Results: " + encryptResult1 + " - " + encryptResult2 + " - " + encryptResult3)
     decryptResult1 = ende.decrypt(encryptResult1)
     decryptResult2 = ende.decrypt(encryptResult2)
     decryptResult3 = ende.decrypt(encryptResult3)
-    # print("Decrypt Results: " + decryptResult1 + " - " + decryptResult2 + " - " + decryptResult3)
-    if (decryptResult1 == phrases[0]
-        and decryptResult2 == phrases[1]
-        and decryptResult3 == phrases[2]):
-        return True
+    assert decryptResult1 == phrases[0]
+    assert decryptResult2 == phrases[1]
+    assert decryptResult3 == phrases[2]
+    if decryptResult1 != phrases[0] or decryptResult2 != phrases[1]:
+        print("Uneven words(Dog, Liver) issue")
+    elif decryptResult3 != phrases[2]:
+        print("Even word(Four) issue")
     else:
-        return False
+        print("Encryption and Decryption: Pass")
+
 
 
 """
 RUN TESTS
 """
-test1 = test_encryption_decryptionn()
-print("Test1-Pass = " + str(test1))
+test_encryption_decryptionn()
 
